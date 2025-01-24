6553aded3d9ab01e278fb3bbc56da2bad53f3344
AnaStranoMostro <122780363+AnaStranoMostro@users.noreply.github.com>
2025-01-24 02:52:03 +0100

    more changes, yay!

---
+++ ClinicalTrialAPI/Helpers/ErrorResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace ClinicalTrialAPI.Helpers;
+
+/// <summary>
+/// Represents an error response containing a list of error messages.
+/// </summary>
+public class ErrorResponse
+{
+    /// <summary>
+    /// Gets or sets the list of error messages.
+    /// </summary>
+    public IList<string> Errors { get; set; }
+}
--- Tests/Controllers/ClinicalTrialMetadatasControllerTests.cs
+++ Tests/Controllers/ClinicalTrialMetadatasControllerTests.cs
@@ -1,10 +1,12 @@
 ﻿using ClinicalTrialAPI.Controllers;
 using ClinicalTrialAPI.Data;
+using ClinicalTrialAPI.Helpers;
 using ClinicalTrialAPI.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Moq;
+using Newtonsoft.Json.Linq;
 
 
 namespace Tests.Controllers
@@ -148,6 +150,68 @@ namespace Tests.Controllers
             Assert.NotNull(createdMetadata);
             Assert.Equal("test-id-4", createdMetadata.TrialId);
             Assert.Equal(31, createdMetadata.TrialDuration);
+        }
+
+        [Fact]
+        public async Task PostClinicalTrialMetadata_JsonDoesntMatchSchema_ExpectedBehavior()
+        {
+            // Arrange
+            string jsonContent = @"
+            {
+                ""trialId"": ""test-id-4"",
+                ""title"": ""Test Title 4"",
+                ""startDate"": ""2023-01-01"",
+                ""endDate"": ""2023-02-01"",
+                ""participants"": 0,
+                ""status"": 42 //the meaning of life! :) 
+           }";
+            Mock<IFormFile> jsonFile = new Mock<IFormFile>();
+            MemoryStream content = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(jsonContent));
+            jsonFile.Setup(f => f.OpenReadStream()).Returns(content);
+            jsonFile.Setup(f => f.FileName).Returns("test.json");
+            jsonFile.Setup(f => f.Length).Returns(content.Length);
+
+
+            // Act
+            ActionResult<ClinicalTrialMetadata> result = await _controller.PostClinicalTrialMetadata(jsonFile.Object);
+
+            // Assert
+            // make sure that the result is BadRequestObjectResult and that the error messages are correct
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+            var badRequestResult = result.Result as BadRequestObjectResult;
+            var errorResponse = badRequestResult?.Value as ErrorResponse;
+            Assert.NotNull(errorResponse);
+            Assert.Contains(errorResponse.Errors, e => e.Contains("Integer 0 is less than minimum value of 1. Path 'participants'"));
+            Assert.Contains(errorResponse.Errors, e => e.Contains("Invalid type. Expected String but got Integer. Path 'status'"));
+        }
+
+
+        [Fact]
+        public async Task PostClinicalTrialMetadata_EndDateBeforeStartDate_ExpectedBehavior()
+        {
+            // Arrange
+            string jsonContent = @"
+            {
+                ""trialId"": ""test-id-X"",
+                ""title"": ""Test Title X"",
+                ""startDate"": ""2023-01-01"",
+                ""endDate"": ""2022-02-01"",
+                ""participants"": 150,
+                ""status"": ""Not Started""
+            }";
+            Mock<IFormFile> jsonFile = new Mock<IFormFile>();
+            MemoryStream content = new MemoryStream(System.Text.Encoding.UTF8.GetBytes(jsonContent));
+            jsonFile.Setup(f => f.OpenReadStream()).Returns(content);
+            jsonFile.Setup(f => f.FileName).Returns("test.json");
+            jsonFile.Setup(f => f.Length).Returns(content.Length);
+
+
+            // Act
+            ActionResult<ClinicalTrialMetadata> result = await _controller.PostClinicalTrialMetadata(jsonFile.Object);
+
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result.Result);
+            Assert.Equal("End date cannot be before start date.", (result.Result as BadRequestObjectResult)?.Value);
         }
 
         [Fact]
